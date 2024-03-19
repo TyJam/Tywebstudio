78dd74b95c03d64e26dbea8028f4f54e1fa86a5b
Tishaya <118938566+Kiyo-io@users.noreply.github.com>
2024-03-19 01:26:45 -0400

    testing javascript functionality

--- Downloads/Tywebstudio-main 2/index.js
+++ Downloads/Tywebstudio-main 2/index.js
@@ -1,1 +1,6 @@
-alert("Hi");
+const pink = document.getElementById("service");
+
+pink.addEventListener("click", () => {
+  pink.classList.toggle("dropdown-content");
+  console.log("I was clicked");
+});
