a834c3b765cbddd3a6a40e62e0654cc8000a1bf5
TyJam <djkoatz@email.com>
2024-11-03 10:00:36 -0500

    update script.js

---
+++ form.php
@@ -0,0 +1,10 @@
+<?php 
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name    =  $_POST['name'];
+    $email   =  $_POST['email'];
+    $phone   =  $_POST['phone'];
+    $company =  $_POST['company'];
+
+      
+
+  }
