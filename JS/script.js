bd0039fbbbf52dad108ccc9d1968fee6e24953a6
Ali-Peter <aliogochukwu06@gmail.com>
2024-11-27 20:44:51 +0100

    pushed logo page

--- JS/script.js
+++ JS/script.js
@@ -254,3 +254,26 @@ function init() {
 init();
 
 window.addEventListener("resize", init);
+
+
+/* LOGO JAVASCRIPT +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/* FAQs section +++++++++++++++++++++*/
+const listItems = document.querySelectorAll('.right li');
+
+listItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const additionalInfoDiv = item.querySelector('.additional-info');
+
+    document.querySelectorAll('.additional-info').forEach(div => {
+      if (div !== additionalInfoDiv) {
+        div.style.display = 'none';
+      }
+    });
+
+    if (additionalInfoDiv.style.display === 'block') {
+      additionalInfoDiv.style.display = 'none';
+    } else {
+      additionalInfoDiv.style.display = 'block';
+    }
+  });
+});
